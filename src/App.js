6aa98cfe16e5a7fdda4de8f47d7b7f9fe7b9acf3
Bruno Silva <bruno3dcontato@gmail.com>
2020-01-31 15:32:41 -0300

    remover horas quando nao disponivel do campo de exibicao da duracao das thumbnails

--- src/App.js
+++ src/App.js
@@ -53,7 +53,12 @@ function App() {
                             onLoadedMetadata={event => setVideoDuration(event.target.duration)}
                         />
                         <div className="overlay-info-container">
-                            <span className="overlay-info-time">{new Date(videoDuration * 1000).toISOString().substr(11, 8)}</span>
+                            <span className="overlay-info-time">
+                                {new Date(videoDuration * 1000)
+                                    .toISOString()
+                                    .substr(11, 8)
+                                    .replace(/00:/, "")}
+                            </span>
                         </div>
                     </>
                 )}
